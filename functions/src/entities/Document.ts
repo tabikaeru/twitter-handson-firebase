7015980655cbc25510ed2e0113b2a560060d3a94
tabikaeru <moriokayuixxx@gmail.com>
2020-08-14 14:48:45 +0900

    feat:Document entity

--- functions/src/entities/Document.ts
+++ functions/src/entities/Document.ts
@@ -4,13 +4,13 @@ export const createDocument = <T>(document: T) => {
   return {
     ...document,
     createdAt: firestore.FieldValue.serverTimestamp(),
-    updatedAt: firestore.FieldValue.serverTimestamp(),
+    updatedAt: firestore.FieldValue.serverTimestamp()
   }
 }
 
 export const updateDocument = <T>(document: T) => {
   return {
     ...document,
-    updatedAt: firestore.FieldValue.serverTimestamp(),
+    updatedAt: firestore.FieldValue.serverTimestamp()
   }
 }
